9658b85c7550734bcfa6d5de3be9ff4c6ef96b2a
pasha <p.korenev@voroninstudio.eu>
2017-03-03 18:20:40 +0200

    form validation

--- app/assets/javascripts/validate_form.js
+++ app/assets/javascripts/validate_form.js
@@ -1,15 +1,17 @@
 $().ready(function(){
-
-  $("form.validate").validate({
-    rules: {
-      firstname: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      phone: "required",
-      address: "required"
-    },
+  $("form.validate").each(function(){
+      $(this).validate({
+          rules: {
+              firstname: "required",
+              email: {
+                  required: true,
+                  email: true
+              },
+              phone: "required",
+              address: "required"
+          },
+      })
   })
+
 
 })
