17458134f0ba57d6945ef404d35512cf4cb4d2c5
pkorenev <p.korenev@voroninstudio.eu>
2017-03-31 22:11:59 +0300

    app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -75,4 +75,23 @@ module ApplicationHelper
         {key: :media, children: [:media_news, :media_blog, :media_video, :media_press]}, 
         :contacts])
   end
+
+  def site_data(k)
+    begin
+      h = YAML.load(IO.read(Rails.root.join("config/site_data.yml").to_s))['site_data']
+      keys = k.split(".")
+      v = h
+      keys.each do |k|
+        v = v[k]
+      end
+      return v
+
+    rescue
+      return nil
+    end
+  end
+
+  def social_links
+    Hash[site_data("social_links").map{|k, v| [k, {icon: "svg/social/#{k}.svg", url: v}] }]
+  end
 end
