b8d321334cd94e4afe7f031299560743f762e1b3
pkorenev <p.korenev@voroninstudio.eu>
2017-05-24 13:20:12 +0300

    app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,7 +71,6 @@ class ApplicationController < ActionController::Base
   def initialize_locale_links
     locale_links = {}
     Cms.config.provided_locales.each do |locale|
-      #url = @page_instance.try{|p| v = p.url(locale); v = p.try(:default_url) if v.blank?; if !v.start_with?("/") then v = "/#{v}" end;   ("/#{locale}/#{v}" ) }
       url = @page_instance.try{ |p| v = p.url(locale); v = p.try(:default_url, locale) if v.blank?; return nil if v.blank?; if !v.start_with?("/") then v = "/#{v}" end;  v }
 
       locale_links[locale.to_sym] = url
