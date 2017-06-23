aec9958ce669bc7beca4166f71eef5d97e334d4c
pkorenev <p.korenev@voroninstudio.eu>
2017-06-23 10:45:57 +0300

    app

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Ekran
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:uk, :ru, :en]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :uk
     config.i18n.fallbacks = [:en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
