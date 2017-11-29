eb1b743c03232d853cb3c970378f7d5bbc2dab8a
pkorenev <p.korenev@voroninstudio.eu>
2017-11-29 22:19:47 +0200

    fix

--- db/migrate/20170309114314_add_sorting_position_to_pages.rb
+++ db/migrate/20170309114314_add_sorting_position_to_pages.rb
@@ -1,5 +1,11 @@
 class AddSortingPositionToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :sorting_position, :integer
+  def up
+    if !column_exists?(:pages, :sorting_position)
+      add_column :pages, :sorting_position, :integer
+    end
+  end
+
+  def down
+    remove_column :pages, :sorting_position
   end
 end
